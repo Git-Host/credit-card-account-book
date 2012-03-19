ee74b6b79f0f433a44c187784537da41340a10d7
kokowct <kokowct@53fe364f-7278-096b-5f1b-3fb73a8961f4>
2012-03-19 10:38:40 +0000

    DataBase Table

---
+++ CardAcountBook/src/kr/ac/hansung/CardDB.java
@@ -0,0 +1,33 @@
+package kr.ac.hansung;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class CardDB extends SQLiteOpenHelper{
+	
+	public CardDB(Context context) {
+		super(context, "card.db",null,1);
+		
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		 db.execSQL("CREATE TABLE card ( name TEXT PRIMARY KEY ,pDay INTEGER, tAmount INTEGER,CardType INTGER);" );
+		 db.execSQL("CREATE TABLE breakdowstats " +
+			 		"( num INTEGER PRIMARY KEY ,cardName TEXT ,FOpYear INTEGER ,pMonth INTGER ,pDay INTGER ,pPlace TEXT" +
+			 		",installment INTGER);" );
+
+
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
