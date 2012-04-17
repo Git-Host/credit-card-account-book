919417301c777bbfe002a005515be0e804b8992b
gazi6024 <gazi6024@53fe364f-7278-096b-5f1b-3fb73a8961f4>
2012-04-17 12:47:50 +0000

    SMS Parser(NH) + DB + Gragph //시연전 최종

--- trunk/CardAcountBook/src/kr/ac/hansung/CardDB.java
+++ trunk/CardAcountBook/src/kr/ac/hansung/CardDB.java
@@ -4,23 +4,24 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class CardDB extends SQLiteOpenHelper{
-	public CardDB(Context context) {                
-		super(context, "card.db",null,1);
-		// TODO Auto-generated constructor stub 
-		}         
+public class CardDB extends SQLiteOpenHelper {
+	public CardDB(Context context) {
+		super(context, "card.db", null, 1);
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		// TODO Auto-generated method stub
-		db.execSQL("CREATE TABLE card ( name TEXT PRIMARY KEY ,pDay INTEGER, tAmount INTEGER,CardType INTGER);" );                 
-		db.execSQL("CREATE TABLE breakdowstats ( num INTEGER PRIMARY KEY ,cardName TEXT ,pYear INTEGER ,pMonth INTGER ,pDay INTGER ,pPlace TEXT" +    
-				",price INTGER, category TEXT);" );
-		
+		db.execSQL("CREATE TABLE card ( name TEXT PRIMARY KEY ,pDay INTEGER, tAmount INTEGER,CardType INTGER);");
+		db.execSQL("CREATE TABLE breakdowstats ( num INTEGER PRIMARY KEY ,cardName TEXT ,pYear INTEGER ,pMonth INTGER ,pDay INTGER ,pPlace TEXT"
+				+ ",price INTGER, category TEXT);");
+
 	}
+
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// TODO Auto-generated method stub
-		
-	} 
-}
 
+	}
+}
